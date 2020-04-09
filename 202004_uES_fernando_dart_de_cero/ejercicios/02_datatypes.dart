24b76df32a00fc2e9f4ba640f1c350eb4fc657e8
ls404 <lubomir@stefanov.biz>
2020-04-10 02:21:27 +0300

    :car:

---
+++ 202004_uES_fernando_dart_de_cero/ejercicios/02_datatypes.dart
@@ -0,0 +1,21 @@
+main() {
+  
+  // Numeros
+  int a = 10;
+  double _b = 10.5;
+  double $c = 10.3;
+  int apt = 12;
+  print("$a, $_b, ${$c}");
+
+
+
+  // Strings - Cadenas de caracteres
+
+
+  // Booleans
+
+  // Listas - Arreglos
+
+  // Sets
+
+}
